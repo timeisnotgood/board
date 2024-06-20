1ae3707efa4ca5ce7891cfe55329fa6c634c80f5
bhoopathi <bhoopathicl@gmail.com>
2024-06-20 18:58:27 +0530

    api enhancement

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -5,29 +5,27 @@ const jwt = require('jsonwebtoken')
 
 const login = async(req, res) =>{
     const { email, password } = req.body;
+    
     if( !email && !password ){
-        res.sendStatus(400)
-        throw new Error(" All Fields are mandatory !! ")
+        res.status(400).json({"error":" All Fields are mandatory !! "})
+        return;
     }
 
     const data = await knex('user').where({'email':email});
-    console.log(data);
     
     if(data == ''){
         res.status(404).json({"status":"User not registered !!"})
         return;
     }
-    else{
-        if(data != '' && (await bcrypt.compare(password, data[0].password))){
+    else if(data[0].email == email){
+        if(await bcrypt.compare(password, data[0].password)){
             const accesstoken = jwt.sign({
                 user:{
                     username : data[0].username,
                     email : data[0].email,
                     id : data[0].id
                 },
-            }, process.env.your_jwt_secret_key, {expiresIn:"10m"})
-            console.log(accesstoken);
-    
+            }, process.env.your_jwt_secret_key, {expiresIn:"10m"})    
             res.json({"status" : "Loged in successfully ","accesstoken":accesstoken})
         }else{
             res.json({"error" : "Incorrect Password"})
@@ -38,7 +36,6 @@ const login = async(req, res) =>{
 const signup = async(req, res) =>{
     try {
         const {username, email, password} = req.body;
-        console.log(req.body);
         const existinguser = await knex('user').where({'email':email});
 
         if (existinguser != '') {
